cf90bb0d369cf2e63ea843a9742b9ca1cf13ca76
prStudentka <bk_nvk@mail.ru>
2023-06-12 07:36:02 +0000

    fix answer even

--- brain_games/games/rule.py
+++ brain_games/games/rule.py
@@ -1,33 +1,17 @@
 def rule_even():
-    # print(" + " * 10)
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
-    # print(" + " * 10)
-    # print()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
 def rule_calc():
-    print(" - " * 8)
     print("What is the result of the expression?")
-    print(" - " * 8)
-    print()
 
 
 def rule_gcd():
-    print(" -- " * 10)
     print("Find the greatest common divisor of given numbers.")
-    print(" -- " * 10)
-    print()
 
 
 def rule_progression():
-    print()
-    print(" # # " * 10)
     print("What number is missing in the progression?")
-    print(" # # " * 10)
-    print()
 
 def rule_prime():
-    print()
-    print(" ~ " * 10)
-    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
-    print()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
