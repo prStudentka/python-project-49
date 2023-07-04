b4f989ab4b3b7550731b5ec1276fccb52ad64982
prStudentka <bk_nvk@mail.ru>
2023-07-04 10:08:43 +0000

    refactor: delete greetings

--- brain_games/game.py
+++ brain_games/game.py
@@ -2,12 +2,8 @@ import prompt
 from brain_games.games.answer import check_answer
 
 
-def greetings():
-    print('Welcome to the Brain Games!')
-
-
 def start_game(game):
-    greetings()
+    print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name?')
     print(f'Hello, {user_name}!')
     print(game.get_rule())
