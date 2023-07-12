e17c927ebe9e0c3b0fc8bdb7bb1128a8847e3c28
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-12 13:49:10 +0300

    ADD length and min_num

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -3,6 +3,8 @@ import random as rnd
 
 __RULE = 'What number is missing in the progression?'
 __MIN_LENGTH = 5
+__MAX_LENGTH = 11
+_MIN_NUM = 1
 
 
 def get_rule():
@@ -10,9 +12,9 @@ def get_rule():
 
 
 def get_pairQA():
-    length = rnd.randint(__MIN_LENGTH, 11)
-    num1 = rnd.randint(1, 15)
-    diff = rnd.randint(1, 11)
+    length = rnd.randint(__MIN_LENGTH, __MAX_LENGTH)
+    num1 = rnd.randint(_MIN_NUM, __MAX_LENGTH + __MIN_LENGTH)
+    diff = rnd.randint(_MIN_NUM, __MAX_LENGTH)
     progression = [i * num1 + diff
                    for i in range(length)]
     pos = rnd.randint(0, length - 1)
