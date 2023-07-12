b45d01376bfdbaf0895f8d1689d9c0aee548fc83
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-12 13:35:47 +0300

    Update MAGIC

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -3,16 +3,17 @@ from operator import sub, mul, add
 
 
 __RULE = 'What is the result of the expression?'
-
+_MIN_NUM = 1
+_MAX_NUM = 10
 
 def get_rule():
     return __RULE
 
 
 def get_pairQA():
-    num1 = rnd.randint(1, 20)
+    num1 = rnd.randint(_MIN_NUM, 2 * _MAX_NUM)
     operation = rnd.choice(['+', '-', '*'])
-    num2 = rnd.randint(0, 10)
+    num2 = rnd.randint(_MIN_NUM - 1, _MAX_NUM)
     if operation == '-' and num2 > num1:
         num1, num2 = num2, num1
     question = f'{num1} {operation} {num2}'
