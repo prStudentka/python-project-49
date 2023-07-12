a27a923e12f2f5e776d697e81b4253836dc41e03
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-12 14:12:31 +0300

    Update answer

--- brain_games/answer.py
+++ brain_games/answer.py
@@ -2,12 +2,6 @@ __ANSWER_LIST = ['yes', 'no']
 __correct = ()
 
 
-def is_correct_answer(answer: str) -> bool:
-    if answer in __ANSWER_LIST:
-        return True
-    return False
-
-
 def has_answer(text: str) -> bool:
     try:
         if len(text.strip()) == 0:
@@ -28,18 +22,12 @@ def check_answer_str(answer: str) -> list:
     if not has_answer(answer):
         return []
     user_answer = answer.lower().strip()
-    __correct = (is_correct_answer(user_answer),)
+    __correct = (user_answer in __ANSWER_LIST,)
     return user_answer, __correct[0]
 
 
 def check_answer_int(answer: str) -> list:
     if not has_answer(answer):
         return []
-    if answer.isnumeric():
-        __correct = (True,)
-    else:
-        __correct = (False,)
+    __correct = (answer.isnumeric(),)
     return int(answer), __correct[0]
-
-
-__all__ = ['check_answer']
