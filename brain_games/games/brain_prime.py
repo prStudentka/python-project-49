377ba4e4bed89517aa655c819a5de73fae586f51
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-24 11:50:41 +0300

    Update prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,18 +4,20 @@ import random as rnd
 __RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 _MIN_NUM = 1
 _MAX_NUM = 120
+_LIMIT_OVER_DOZEN = 11
+_LIMIT = 10
 
 
 def get_rule():
     return __RULE
 
 
-def is_prime(number: str) -> bool:
-    if int(number) == 1 or not int(number):
+def is_prime(number: int) -> bool:
+    if number == 1 or not number:
         return False
-    length = int(number) // 2 + 1 if int(number) <= 10 else 11
-    for i in range(2, length):
-        if int(number) % i == 0:
+    end = number // 2 + 1 if number <= _LIMIT else _LIMIT_OVER_DOZEN
+    for i in range(2, end):
+        if number % i == 0:
             return False
     return True
 
